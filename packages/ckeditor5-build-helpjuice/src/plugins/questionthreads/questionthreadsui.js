a59e6cc42f245f5920771c925d2dcd2a39575d1a
Nicholas Jarretta <nicholas@jarretta.com>
2022-05-05 15:48:58 -0300

    Remove unnecessary icon

--- packages/ckeditor5-build-helpjuice/src/plugins/questionthreads/questionthreadsui.js
+++ packages/ckeditor5-build-helpjuice/src/plugins/questionthreads/questionthreadsui.js
@@ -1,6 +1,5 @@
 import ButtonView from '@ckeditor/ckeditor5-ui/src/button/buttonview';
 import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
-import CommentIcon from './icons/comment.svg';
 
 export default class QuestionThreadsUI extends Plugin {
     init() {}
