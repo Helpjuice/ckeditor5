065c97f6127aeca9f9d7c4fd89eb820a40f6a933
Gabriel Santo <gvgesanto2@gmail.com>
2023-05-08 06:45:29 -0300

    feat: Show modal on correct position

--- packages/ckeditor5-build-helpjuice-v2/src/plugins/glossary/glossaryui.js
+++ packages/ckeditor5-build-helpjuice-v2/src/plugins/glossary/glossaryui.js
@@ -1,5 +1,5 @@
-import ButtonView from '@ckeditor/ckeditor5-ui/src/button/buttonview';
-import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+import { ButtonView } from 'ckeditor5/src/ui';
+import { Plugin } from 'ckeditor5/src/core';
 import BookIcon from './icons/book-icon.svg';
 
 export default class GlossaryUI extends Plugin {
@@ -7,61 +7,61 @@ export default class GlossaryUI extends Plugin {
 		const editor = this.editor;
 		const t = editor.t;
 
-		editor.ui.componentFactory.add('glossary', locale => {
-			const buttonView = new ButtonView(locale);
+		editor.ui.componentFactory.add( 'glossary', locale => {
+			const buttonView = new ButtonView( locale );
 
-			buttonView.set({
-				label: t('Insert Glossary'),
+			buttonView.set( {
+				label: t( 'Insert Glossary' ),
 				icon: BookIcon,
 				tooltip: true,
-				class: "insert-glossary-btn"
-			});
+				class: 'insert-glossary-btn'
+			} );
 
 			// Execute the command when the button is clicked (executed).
-			this.listenTo(buttonView, 'execute', () => {
-				const glossaryModal = document.getElementById("new-glossary-term-modal");
-				let glossaryTermExpression = glossaryModal.querySelector("#glossary_term_expression");
-				let glossaryTermDefinition = glossaryModal.querySelector("#glossary_term_definition");
+			this.listenTo( buttonView, 'execute', () => {
+				const glossaryModal = document.getElementById( 'new-glossary-term-modal' );
+				const glossaryTermExpression = glossaryModal.querySelector( '#glossary_term_expression' );
+				const glossaryTermDefinition = glossaryModal.querySelector( '#glossary_term_definition' );
 
-				const range = editor.model.document.selection.getFirstRange()
-				let selection = "";
-				for (const item of range.getItems()) {
-					if (item.data) {
+				const range = editor.model.document.selection.getFirstRange();
+				let selection = '';
+				for ( const item of range.getItems() ) {
+					if ( item.data ) {
 						selection += item.data;
 					}
 				}
 
-				if (selection.length) {
+				if ( selection.length ) {
 					// WE USE `window.getSelection()` BECAUSE EDITOR SELECTION DOESN'T ACCEPT `getBoundingClientRect`
-					const position = window.getSelection().getRangeAt(0).getBoundingClientRect();
-					// SET BODY TO FOCUS MODE AND SHOW MODAL ON CORRECT POSITION
-					document.body.classList.add("focus-mode");
-					glossaryModal.style.display = "block";
-					glossaryModal.style.top = position.top + 20 + "px";
-					glossaryModal.style.left = position.left - 165 + position.width/2 + "px";
+					const range = window.getSelection().getRangeAt( 0 );
+					const position = range.getBoundingClientRect();
+					const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+					const selectionHeight = position.bottom - position.top;
+					const maxLeftPost = document.querySelector( '.editor-content-new .editor-body' ).getBoundingClientRect().right - ( glossaryModal.offsetWidth + 40 );
+
+					// SHOW MODAL ON CORRECT POSITION
+					glossaryModal.style.display = 'block';
+					glossaryModal.style.top = position.top + scrollTop + selectionHeight + 8 + 'px';
+					glossaryModal.style.left = position.left > maxLeftPost ? `${ maxLeftPost }px` : `${ position.left }px`;
 					// SET TERM EXPRESSION
 					glossaryTermExpression.value = selection.trim();
-					glossaryTermDefinition.value = "";
-					// WE NEED TO USE SET TIMEOUT IN ORDER TO FOCUS TEXTAREA
-					setTimeout(() => {
-						glossaryTermDefinition.focus();
-					}, 10);
+					glossaryTermDefinition.value = '';
 				} else {
-					this.createMessage('comment-message error', 'You must select some text in order to create a glossary item');
+					this.createMessage( 'comment-message error', 'You must select some text in order to create a glossary item' );
 				}
-			});
+			} );
 
 			return buttonView;
-		});
+		} );
 	}
 
-	createMessage(classes, text) {
-		let message = document.createElement("div");
+	createMessage( classes, text ) {
+		const message = document.createElement( 'div' );
 		message.className = classes;
 		message.textContent = text;
-		document.getElementById("article-comments").append(message);
-		message.style.display = "block";
+		document.getElementById( 'article-comments' ).append( message );
+		message.style.display = 'block';
 
-		setTimeout(() => message.remove(), 3000);
+		setTimeout( () => message.remove(), 3000 );
 	}
 }
