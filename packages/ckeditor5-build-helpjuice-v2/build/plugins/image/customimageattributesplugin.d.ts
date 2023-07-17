ed4aaf0be4d3c3f32446e8930bbf113e27a0a175
bondok6 <kyrilloshany99@gmail.com>
2023-07-17 23:30:46 +0300

    Rebase & Rebuild

--- packages/ckeditor5-build-helpjuice-v2/build/plugins/image/customimageattributesplugin.d.ts
+++
@@ -1,7 +0,0 @@
-export default class CustomImageAttributesPlugin extends Plugin {
-    static get requires(): (typeof ImageBlock | typeof ImageInline)[];
-    init(): void;
-}
-import Plugin from "@ckeditor/ckeditor5-core/src/plugin";
-import ImageBlock from "@ckeditor/ckeditor5-image/src/imageblock";
-import ImageInline from "@ckeditor/ckeditor5-image/src/imageinline";
