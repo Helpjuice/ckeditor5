9c0fa39fcbafd273072bfdb5d760ab3d68e632b5
bondok6 <kirolousbondok6@gmail.com>
2023-08-17 22:44:14 +0300

    Remove inlineCode custom plugin

--- packages/ckeditor5-build-helpjuice-v2/build/plugins/inlinecode/inlinecodeui.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @license Copyright (c) 2003-2023, CKSource Holding sp. z o.o. All rights reserved.
- * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
- */
-import { Plugin } from 'ckeditor5/src/core';
-export default class InlineCodeUI extends Plugin {
-    static get pluginName(): 'InlineCodeUI';
-    init(): void;
-}
